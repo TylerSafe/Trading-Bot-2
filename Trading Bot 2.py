9fc3d6a797868bc43fd21d3bca531ee6d1e8e947
TylerSafe <98946533+TylerSafe@users.noreply.github.com>
2022-05-29 22:34:00 +0800

    Add files via upload
    - Added put option buying
    - Spread is now accurately calculated each minute
    - Average spread added for reference

--- Trading Bot 2.py
+++ Trading Bot 2.py
@@ -4,35 +4,42 @@
 class JumpingRedElephant(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2022, 5, 13)  # Set Start Date
-        self.SetEndDate(2022, 5, 18)  # Set Start Date
+        self.SetStartDate(2021, 1, 1)  # Set Start Date
+        self.SetEndDate(2022, 1, 1)  # Set Start Date
         self.SetCash(25000)  # Set Strategy Cash
+        self.watch_list = ["AMD", "BBBY"]
+        
+        PORTFOLIO = len(self.watch_list)
         
         self.stocks = []
         self.symbols = []
         self.average_vol = []
-        self.volume_one = [0] * 2 # initialize volume variables
-        self.volume_two = [0] * 2
-        self.spread = [0] * 2
-        self.minutes_passed = [0] * 2
+        self.volume_one = [0] * PORTFOLIO # initialize volume variables
+        self.volume_two = [0] * PORTFOLIO
+        self.current_price = [0] * PORTFOLIO
+        self.previous_price = [0] * PORTFOLIO
+        self.minute_window = RollingWindow[TradeBar](1)
+        self.current_high = [0] * PORTFOLIO
+        self.current_low = [0] * PORTFOLIO
+        self.spread = [0] * PORTFOLIO
+        self.total_spread = [0] * PORTFOLIO
+        self.spread_inputs = [0] * PORTFOLIO
+        self.avg_spread = [0] * PORTFOLIO
         
-        for ticker in ["AMD", "BBBY"]:
-            option = self.AddOption(ticker)
+        for i in range(len(self.watch_list)):
+            self.stock_reference = i
+            
+            option = self.AddOption(self.watch_list[i])
             self.symbols.append(option.Symbol)
             option.SetFilter(-2, 2, timedelta(0), timedelta(8)) # get options contracts within 2 strikes that expire between 0 and 8 days from today
             
-            stock = self.AddEquity(ticker, Resolution.Minute).Symbol # also store stock information for price and volume tracking
+            stock = self.AddEquity(self.watch_list[i], Resolution.Minute).Symbol # also store stock information for price and volume tracking
             self.stocks.append(stock)
             
             volume = self.History(stock, 1, Resolution.Daily).volume # get the volume of the previous day
             avg_vol = volume / 390 # get the average for minutely volume
             self.average_vol.append(avg_vol)
-            
-            # a little hacky (used from forum) but works to track price spread in minutely time frame
-            thirtyMinuteConsolidator = TradeBarConsolidator(timedelta(minutes=1))
-            self.SubscriptionManager.AddConsolidator(stock, thirtyMinuteConsolidator)
-            thirtyMinuteConsolidator.DataConsolidated += self.OnThirtyMinuteBar
-            self.barWindow = RollingWindow[TradeBar](1)
+
         
         self.Schedule.On(self.DateRules.EveryDay(self.symbols[0]), self.TimeRules.BeforeMarketClose(self.symbols[0], 10), self.ExitPositions) # exit all positions 15 mins before close
         self.Schedule.On(self.DateRules.EveryDay(self.symbols[0]), self.TimeRules.At(9, 15), self.ResetData)
@@ -40,22 +47,37 @@ class JumpingRedElephant(QCAlgorithm):
 
     def OnData(self, data: Slice):
 
-        if self.Time > self.Time.replace(hour=15, minute=39): # don't trade in the last 20 mins of the day
+        if self.Time > self.Time.replace(hour=15, minute=39) or self.Time < self.Time.replace(hour=9, minute=34): # don't trade in the last 20 mins or first 5 of the day
             return
-        
-        margin = self.Portfolio.MarginRemaining # remaining buying power of account
             
+        margin = self.Portfolio.MarginRemaining # remaining buying power of account
+        
         for i in range(len(self.stocks)): # get the volume for each ticker we are tracking
-            try: # added due to an error when information is not received
-                self.volume_one[i] = self.History(self.stocks[i], 1, Resolution.Minute).volume # get volume of the last minute
+            try: # avoid error by skipping stock if no data available
+            
+                try: # added due to an error when information is not received
+                    self.volume_one[i] = self.History(self.stocks[i], 1, Resolution.Minute).volume # get volume of the last minute
+                except:
+                    self.volume_one[i] = 0 # if error occurs set volume to 0 so so it is not used
+                
+                self.minute_window.Add(data[self.watch_list[i]]) # get data for each stock in watchlist
+                self.current_high[i] = self.minute_window[self.minute_window.Count-1].High # get the max price of last minute
+                self.current_low[i] = self.minute_window[self.minute_window.Count-1].Low # get the min price of last minute
+                self.spread[i] = self.current_high[i] - self.current_low[i] # calculate the spread
+                self.total_spread[i] += self.spread[i]
+                self.spread_inputs[i] += 1
+                
+                self.current_price[i] = data[self.watch_list[i]].Price
             except:
-                self.volume_one[i] = 0 # if error occurs set volume to 0 so so it is not used
+                pass
             
         for i in range(len(self.symbols)):
             strat1 = False
             
             # strat1 SOS, high volume, low spread, red candle
-            if float(self.volume_one[i]) > float(self.average_vol[i] * 1.5):
+            self.avg_spread[i] = float(self.total_spread[i]) / float(self.spread_inputs[i])
+            
+            if float(self.volume_one[i]) > float(self.average_vol[i] * 1.5) and float(self.spread[i]) < (self.avg_spread[i] * 0.6) and self.current_price[i] < self.previous_price[i] and float(self.current_price[i]) != 0:
                 strat1 = True
             
             if strat1 and float(self.volume_one[i]) != 0 and float(self.volume_two[i]) != 0 \
@@ -67,11 +89,16 @@ class JumpingRedElephant(QCAlgorithm):
         
         for i in range(len(self.stocks)):
             self.volume_two[i] = self.volume_one[i] # sets volume of 2nd minute to volume of first minute before next ondata
+            self.previous_price[i] = self.current_price[i]
             
         # Volume chart
         self.Plot("Average Volume", "Volume", self.volume_one[0])
         self.Plot("Average Volume", "standard", self.average_vol[0])
         self.Plot("Average Volume", "x1.5", (self.average_vol[0] * 1.5))
+        
+        self.Plot("spread", "current", self.spread[0])
+        self.Plot("spread", "avg", self.avg_spread[0])
+        self.Plot("spread", "avg * 0.7", (self.avg_spread[0] * 0.7))
         
     def ExitPositions(self):
         self.Liquidate() # exit all positions
@@ -84,20 +111,7 @@ class JumpingRedElephant(QCAlgorithm):
             avg_vol = volume / 390 # get the average for minutely volume
             self.average_vol.append(avg_vol)
             
-    # code used from forums, rolling window that plots 1 minute spread
-    def OnThirtyMinuteBar(self, sender, bar):
-        self.barWindow.Add(bar)
-        
-        if not self.barWindow.IsReady:
-            return
- 
-        currentBar = self.barWindow[0]
-        
-        currentHigh = currentBar.High
-        currentLow = currentBar.Low
-        
-        self.Plot("My Custom Chart", "High", currentHigh)
-        self.Plot("My Custom Chart", "Low", currentLow)
+
         
     def BuyCall(self, chains): # buy an ATM call (market order need to change to limit)
         expiry = sorted(chains, key = lambda x: x.Expiry, reverse = True)[0].Expiry # get the furthest expiration date (greater than 0, less than 8)
